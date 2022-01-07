f94443d93f283db177fba70384dd1ef543347680
austingardner <gardneraustin@protonmail.com>
2022-01-07 02:04:21 -0500

    wip

--- src/services/create/index.js
+++ src/services/create/index.js
@@ -1,8 +1,8 @@
-import template from "../../pages/index";
+import fetch from 'node-fetch';
 
 export default async (req, res) => {
-  
-  await fetch('localhost:5000/data', {method: 'POST', body: JSON.stringify({title: req.body.title, text: req.body.text})});
+  console.log(req);
+  await fetch('http://localhost:5000/data', {method: 'POST', body: JSON.stringify({title: req.body.title, text: req.body.text})});
   //res.json(Promise.resolve({success: true}));
   res.redirect('/');
 };
--- src/services/getPosts/index.js
+++ src/services/getPosts/index.js
@@ -1,4 +1,6 @@
+import fetch from 'node-fetch';
+
 export default async (req, res) => {
-    const data = await fetch('localhost:5000/data');
+    const data = await fetch('http://localhost:5000/data').then(d => d.json());
     return data;
-  };
+};
