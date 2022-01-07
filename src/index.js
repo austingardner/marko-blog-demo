01638cbf9f3fe926de0019bf34b17119c545b2e5
austingardner <gardneraustin@protonmail.com>
2022-01-06 22:27:19 -0500

    wip

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import { Router } from "express";
 import indexPage from "./pages/index";
+import createPage from "./pages/create";
+import viewPage from "./pages/view";
 import usersService from "./services/users";
 
 export default Router()
   .get("/", indexPage)
+  .get("/create", createPage)
+  .get("/view", viewPage)
   .get("/services/users", usersService);
